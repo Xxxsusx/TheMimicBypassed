24f4ebafadd1c0868ad1a55d743e94b074c75d4a
Xxxsusx <105179663+Xxxsusx@users.noreply.github.com>
2023-06-02 18:58:46 -0300

    Update walkspeedbypass.lua

--- walkspeedbypass.lua
+++ walkspeedbypass.lua
@@ -6,15 +6,15 @@ screenGui.Parent = game.Players.LocalPlayer.PlayerGui
 local frame = Instance.new("Frame")
 frame.Name = "ButtonFrame"
 frame.Size = UDim2.new(0, 200, 0, 80)
-frame.Position = UDim2.new(0.5, -100, 0.5, -40)
+frame.Position = UDim2.new(0, 20, 1, -100) -- Bottom left corner
 frame.BackgroundColor3 = Color3.new(0, 0, 0) -- Black background
 frame.Parent = screenGui
 
 -- Create a TextButton inside the frame for enable/disable
 local toggleButton = Instance.new("TextButton")
 toggleButton.Name = "ToggleButton"
-toggleButton.Size = UDim2.new(0, 150, 0, 40)
-toggleButton.Position = UDim2.new(0, 25, 0, 20)
+toggleButton.Size = UDim2.new(0.5, -10, 1, -10) -- Smaller size with padding
+toggleButton.Position = UDim2.new(0, 5, 0, 5) -- Padding
 toggleButton.BackgroundColor3 = Color3.new(0, 1, 0) -- Green background
 toggleButton.TextColor3 = Color3.new(1, 1, 1) -- White text color
 toggleButton.Font = Enum.Font.GothamBold -- Set font to GothamBold
@@ -26,7 +26,7 @@ toggleButton.Parent = frame
 local textBox = Instance.new("TextBox")
 textBox.Name = "SpeedTextBox"
 textBox.Size = UDim2.new(0, 150, 0, 25)
-textBox.Position = UDim2.new(0, 25, 0, 65)
+textBox.Position = UDim2.new(0.5, 5, 0, 35) -- Below the toggle button
 textBox.BackgroundColor3 = Color3.new(1, 1, 1) -- White background
 textBox.TextColor3 = Color3.new(0, 0, 0) -- Black text color
 textBox.Font = Enum.Font.GothamBold -- Set font to GothamBold
@@ -61,14 +61,19 @@ end
 -- Function to toggle the walkspeed script
 local function toggleWalkspeedScript()
     walkspeedEnabled = not walkspeedEnabled -- Toggle the state
+    local speed = tonumber(textBox.Text)
 
     if walkspeedEnabled then
         -- Enable the script
-        local speed = tonumber(textBox.Text)
         if speed and speed >= MIN_SPEED and speed <= MAX_SPEED then
             toggleButton.Text = "DISABLED"
             toggleButton.BackgroundColor3 = Color3.new(1, 0, 0) -- Red background
-            setWalkspeed(game.Players.LocalPlayer, speed)
+            while walkspeedEnabled do
+                for _, player in ipairs(game.Players:GetPlayers()) do
+                    setWalkspeed(player, speed)
+                end
+                wait(0.03) -- Loop interval of 0.03 seconds
+            end
         else
             walkspeedEnabled = false -- Invalid speed, revert back to disabled state
             textBox.Text = ""
@@ -77,7 +82,11 @@ local function toggleWalkspeedScript()
         -- Disable the script
         toggleButton.Text = "ENABLE"
         toggleButton.BackgroundColor3 = Color3.new(0, 1, 0) -- Green background
-        setWalkspeed(game.Players.LocalPlayer, MIN_SPEED)
+        for _, player in ipairs(game.Players:GetPlayers()) do
+            if player.Character and player.Character:FindFirstChild("Humanoid") and player.Character.Humanoid.Health > 0 then
+                player.Character.Humanoid.WalkSpeed = MIN_SPEED -- Set to minimum walkspeed
+            end
+        end
     end
 end
 
@@ -86,11 +95,11 @@ local function toggleMinimize()
     if frame.Size == UDim2.new(0, 200, 0, 80) then
         frame.Size = UDim2.new(0, 100, 0, 40)
         minimizeButton.Text = "+"
-        minimizeButton.Position = UDim2.new(1, -25, 0, 15) -- Top right corner
+        minimizeButton.Position = UDim2.new(1, -25, 1, -25) -- Bottom right corner
     else
         frame.Size = UDim2.new(0, 200, 0, 80)
         minimizeButton.Text = "-"
-        minimizeButton.Position = UDim2.new(1, -25, 1, -25) -- Bottom right corner
+        minimizeButton.Position = UDim2.new(1, -25, 0, 15) -- Top right corner
     end
 end
 
